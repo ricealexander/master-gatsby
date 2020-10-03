c0d00832bb892cba95d8cb42d42ea62425150e0e
Alexander Rice <webdev@ricealexander.com>
2020-10-03 18:26:35 -0500

    :lipstick: Create Global CSS
    :seven: Global Styles

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Footer from './Footer'
 import Navigation from './Navigation'
+import GlobalStyles from '../styles/GlobalStyles'
+
+import 'normalize.css'
 
 function Layout ({ children }) {
   return (
     <div>
+      <GlobalStyles />
       <Navigation />
       {children}
       <Footer />
---
+++ gatsby/src/styles/GlobalStyles.js
@@ -0,0 +1,86 @@
+import { createGlobalStyle } from 'styled-components'
+import bg from '../assets/images/bg.svg'
+import stripes from '../assets/images/stripes.svg'
+
+const GlobalStyles = createGlobalStyle`
+  :root {
+    --red: #FF4949;
+    --black: #2E2E2E;
+    --yellow: #ffc600;
+    --white: #fff;
+    --grey: #efefef;
+  }
+
+  html {
+    background-image: url(${bg});
+    background-attachment: fixed;
+    background-size: 450px;
+    font-size: 10px;
+  }
+
+  body {
+    font-size: 2rem;
+  }
+
+  fieldset {
+    border-color: rgba(0,0,0,0.1);
+    border-width: 1px;
+  }
+
+  button {
+    background: var(--red);
+    border: 0;
+    border-radius: 2px;
+    box-shadow: var(--cast) var(--cast) 0 var(--grey);
+    color: white;
+    cursor: pointer;
+    padding: 0.6rem 1rem;
+    text-shadow: 0.5px 0.5px 0 rgba(0,0,0,0.2);
+    transition: all 0.2s;
+    --cast: 2px;
+    &:hover {
+      --cast: 4px;
+    }
+  }
+
+  .gatsby-image-wrapper img[src*=base64\\,] {
+    image-rendering: -moz-crisp-edges;
+    image-rendering: pixelated;
+  }
+
+  /* Scrollbar Styles */
+  body::-webkit-scrollbar {
+    width: 12px;
+  }
+  html {
+    scrollbar-color: var(--red) var(--white);
+    scrollbar-width: thin;
+  }
+  body::-webkit-scrollbar-track {
+    background: var(--white);
+  }
+  body::-webkit-scrollbar-thumb {
+    background-color: var(--red) ;
+    border: 3px solid var(--white);
+    border-radius: 6px;
+  }
+
+  hr {
+    background-image: url(${stripes});
+    background-size: 1500px;
+    border: 0;
+    height: 8px;
+  }
+
+  img {
+    max-width: 100%;
+  }
+
+  .tilt {
+    display: inline-block;
+    position: relative;
+    transform: rotate(-2deg);
+  }
+`
+
+export default GlobalStyles
